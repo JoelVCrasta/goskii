77402f967e97deefcb4cf64ec0122fccbcf9ecb1
Joel Crasta <loejstarc@gmail.com>
2024-12-02 19:20:01 +0530

    added image to ascii generator function

---
+++ converter/imageconvertor.go
@@ -0,0 +1,10 @@
+package converter
+
+import (
+	"github.com/JoelVCrasta/goskii/generator"
+	"github.com/JoelVCrasta/goskii/imageutils"
+)
+
+func ImageToASCII(path string) {
+	
+}
---
+++ generator/framegen.go
@@ -0,0 +1,37 @@
+package generator
+
+import (
+	"image"
+	"image/color"
+	"strings"
+)
+
+// ASCII character sets to represent the image.
+var (
+	// The characters are ordered from darkest to lightest.
+	CHARSET_1 = []string{".", ":", "-", "+", "*", "?", "#", "%", "$", "@"}
+
+	// The characters are ordered from lightest to darkest.
+	CHARSET_2 = []string{"@", "$", "%", "#", "?", "*", "+", "-", ":", "."}
+)
+
+func GenerateASCII(img image.Gray, width, height int, charset []string) string {
+	var builder strings.Builder
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			pixel := img.GrayAt(x, y)
+			builder.WriteString(getASCIIChar(pixel, charset))
+		}
+		builder.WriteString("\n")
+	}
+
+	return builder.String()
+}
+
+func getASCIIChar(c color.Gray, charset []string ) string {
+	gray := c.Y
+	normalized := int(float64(gray) / 255.0 * float64(len(charset)-1))
+
+	return CHARSET_1[normalized]
+}

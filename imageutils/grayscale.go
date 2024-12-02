fb0e3ec4f0c64813051f8dce7ba24e90bded8033
Joel Crasta <loejstarc@gmail.com>
2024-12-02 19:18:18 +0530

    added grayscale code

---
+++ imageutils/grayscale.go
@@ -0,0 +1,23 @@
+package imageutils
+
+import (
+	"image"
+	"image/color"
+)
+
+func Grayscale(img image.Image) *image.Gray {
+	bounds := img.Bounds()
+	grayImg := image.NewGray(bounds)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			originalColor := img.At(x, y)
+			r, g, b, _ := originalColor.RGBA()
+			lum := 0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b)
+			pixel := color.Gray{uint8(lum / 256)}
+			grayImg.Set(x, y, pixel)
+		}
+	}
+
+	return grayImg
+}	

b21b086e5c1db02c6cb3f8925ffb486825e95fed
Joel Crasta <loejstarc@gmail.com>
2024-12-10 21:18:53 +0530

    added check for png images and appropriate calls

--- convertor/image_convertor.go
+++ convertor/image_convertor.go
@@ -1,4 +1,4 @@
-package converter
+package convertor
 
 import (
 	"fmt"
@@ -7,6 +7,26 @@ import (
 	"github.com/JoelVCrasta/goskii/imageutils"
 )
 
+// Converts the Non Alpha image to ASCII.
+func imageRGB(imageData *imageutils.ImageData, width int, height int) {
+	imageGray := imageutils.Grayscale(imageData.Image)
+	resizedImage := imageutils.BilinearResizeGray(imageGray, width, height)
+
+	ascii := generator.GenerateASCII(resizedImage, width, height, generator.CHARSET_1)
+	fmt.Println(ascii)
+}
+
+// Converts the Alpha image to ASCII. This is for PNG images.
+func imageRGBA(imageData *imageutils.ImageData, width int, height int) {
+	imageGray, alpha := imageutils.GrayscaleAlpha(imageData.Image)
+	resizedImage := imageutils.BilinearResizeGray(imageGray, width, height)
+	alpha = imageutils.ResizeAlpha(alpha, imageData.Width, imageData.Height, width, height)
+
+	ascii := generator.GenerateASCIIAlpha(resizedImage, alpha, width, height, generator.CHARSET_1)
+	fmt.Println(ascii)
+}
+
+// Converts the image to ASCII by calling the appropriate function based on the image extension.
 func ImageToASCII(path string) error {
 	imageData, err := imageutils.LoadImage(path)
 	if err != nil {
@@ -18,12 +38,11 @@ func ImageToASCII(path string) error {
 		return fmt.Errorf("bounds error: %v", err)
 	}
 
-	imageGray := imageutils.Grayscale(imageData.Image)
-	resizedImage := imageutils.BilinearResizeGray(imageGray, imageData.Bounds, width, height)
-
-	ascii := generator.GenerateASCII(resizedImage, width, height, generator.CHARSET_1)
-
-	fmt.Println(ascii)
+	if imageData.Extension == "png" {
+		imageRGBA(imageData, width, height)
+	} else {
+		imageRGB(imageData, width, height)
+	}
 
 	return nil
 }

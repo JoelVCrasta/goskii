937c39f37ba4a491873f863df86d011ed977aa35
Joel Crasta <loejstarc@gmail.com>
2024-12-10 21:18:23 +0530

    added function to get charset and generate ascii function for alpha images

--- generator/ascii_generator.go
+++ generator/ascii_generator.go
@@ -15,6 +15,14 @@ var (
 	CHARSET_2 = []string{"@", "$", "%", "#", "?", "*", "+", "-", ":", "."}
 )
 
+func getASCIIChar(c color.Gray, charset []string ) string {
+	gray := c.Y
+	normalized := int(float64(gray) / 255.0 * float64(len(charset)-1))
+
+	return CHARSET_1[normalized]
+}
+
+
 func GenerateASCII(img *image.Gray, width, height int, charset []string) string {
 	var builder strings.Builder
 
@@ -29,9 +37,21 @@ func GenerateASCII(img *image.Gray, width, height int, charset []string) string 
 	return builder.String()
 }
 
-func getASCIIChar(c color.Gray, charset []string ) string {
-	gray := c.Y
-	normalized := int(float64(gray) / 255.0 * float64(len(charset)-1))
+func GenerateASCIIAlpha(img *image.Gray, alpha [][]uint8, width, height int, charset []string) string {
+	var builder strings.Builder
 
-	return CHARSET_1[normalized]
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			pixel := img.GrayAt(x, y)
+			alphaValue := alpha[y][x]
+			if alphaValue == 0 {
+				builder.WriteString(" ")
+			} else {
+				builder.WriteString(getASCIIChar(pixel, charset))
+			}
+		}
+		builder.WriteString("\n")
+	}
+
+	return builder.String()
 }

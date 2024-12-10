df7f54ed916e622c814838b1aed73c2f3430162c
Joel Crasta <loejstarc@gmail.com>
2024-12-10 21:19:32 +0530

    main now calls ImageToASCII

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/JoelVCrasta/goskii/cmd"
+	"github.com/JoelVCrasta/goskii/convertor"
 )
 
 func main() {
 	cmd.Execute()
 
+	pFlag, _ := cmd.GetCommands()
+	
+	err := convertor.ImageToASCII(pFlag)
+	if err != nil {
+		fmt.Print(err)
+	}
 }

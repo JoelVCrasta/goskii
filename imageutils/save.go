be3b9ca9a6336605fe85aa4c56ae4ffc12a86f21
Joel Crasta <loejstarc@gmail.com>
2024-12-13 20:23:46 +0530

    added save ascii to text file

---
+++ imageutils/save.go
@@ -0,0 +1,21 @@
+package imageutils
+
+import (
+	"fmt"
+	"os"
+)
+
+func SaveToTextFile(ascii string, path string, fileName string) error {
+	file, err := os.Create(fmt.Sprintf("%s/%s.txt", path, fileName))
+	if err != nil {
+		return fmt.Errorf("error creating file: %v", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(ascii)
+	if err != nil {
+		return fmt.Errorf("error writing to file: %v", err)
+	}
+
+	return nil
+}

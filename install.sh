bf3e5750dab7f84c4e2b484ba4c2e4c1e761586f
Joel Crasta <loejstarc@gmail.com>
2025-02-25 22:13:07 +0530

    updated bash script for linux and macos

--- install.sh
+++ install.sh
@@ -30,13 +30,62 @@ if [[ -z "$LATEST_VERSION" ]]; then
     exit 1
 fi
 
+# Download dependencies
+echo "Downloading dependencies..."
+
+# Check if ffmpeg is installed
+if ! command -v ffmpeg &> /dev/null; then
+    echo "FFmpeg not found, downloading latest FFmpeg version..."
+    FFMPEG_URL="https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip"
+
+    if ! sudo curl -sL $FFMPEG_URL -o /tmp/ffmpeg.zip; then
+        echo "Failed to download FFmpeg."
+        exit 1
+    fi
+
+    sudo unzip -o /tmp/ffmpeg.zip -d /tmp
+    sudo cp /tmp/ffmpeg-*-essentials_build/bin/ffmpeg $BIN_DIR
+    sudo rm -rf /tmp/ffmpeg-*-essentials_build /tmp/ffmpeg.zip
+    sudo chmod +x $BIN_DIR/ffmpeg
+else 
+    echo "FFmpeg is already installed."
+fi
+
+# Check if yt-dlp is installed
+if ! command -v yt-dlp &> /dev/null; then
+    echo "yt-dlp not found, downloading latest yt-dlp version..."
+
+    if [[ "$OS" == "linux" ]]; then
+        YT_DLP_URL=$(curl -s https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest | jq -r '.assets[] | select(.name == "yt-dlp_linux") | .browser_download_url')
+    elif [[ "$OS" == "darwin" ]]; then
+        YT_DLP_URL=$(curl -s https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest | jq -r '.assets[] | select(.name == "yt-dlp_macos") | .browser_download_url')
+    fi
+
+    if [[ -z "$YT_DLP_URL" ]]; then
+        echo "Failed to fetch the latest version of yt-dlp."
+        exit 1
+    fi
+
+    if ! sudo curl -sL $YT_DLP_URL -o $BIN_DIR/yt-dlp; then
+        echo "Failed to download yt-dlp."
+        exit 1
+    fi
+
+    sudo chmod +x $BIN_DIR/yt-dlp
+else
+    echo "yt-dlp is already installed."
+fi
+
 # Build Download URL
 BINARY="${APP_NAME}-${OS}-amd64"
 DOWNLOAD_URL="https://github.com/${REPO}/releases/download/${LATEST_VERSION}/${BINARY}"
 
 # Download and Install
 echo "Downloading $APP_NAME from $DOWNLOAD_URL..."
-sudo curl -sL $DOWNLOAD_URL -o $BIN_DIR/$APP_NAME
+if ! sudo curl -sL $DOWNLOAD_URL -o $BIN_DIR/$APP_NAME; then
+    echo "Failed to download $APP_NAME."
+    exit 1
+fi
 
 # Make Binary Executable
 echo "Granting executable permissions to $APP_NAME..."
@@ -50,4 +99,3 @@ else
     echo "Failed to install $APP_NAME."
     exit 1
 fi
-

24d7862bed5722a71db7d7e075fd4a19109d6d33
Joel Crasta <loejstarc@gmail.com>
2025-02-25 22:09:58 +0530

    removed help code from main'

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/JoelVCrasta/goskii/cmd"
 	"github.com/JoelVCrasta/goskii/convertor"
@@ -12,15 +11,6 @@ import (
 func main() {
 	cmd.Execute()
 	cmdFlags := cmd.GetCommands()
-
-	if len(os.Args) > 1 {
-		for _, arg := range os.Args[1:] {
-			if arg == "-h" || arg == "--help" {
-				return
-			}
-		}
-	}
-
 	ftype := cmd.GetFileType()
 
 	if cmdFlags.Path != "" {
